71a1b1cbcbdb34d50918ed2150e64f5f065fc1a6
EduardoLussi <eduardolussi@gmail.com>
2020-09-26 11:40:32 -0300

    get fail

--- src/views/Devices.js
+++ src/views/Devices.js
@@ -13,6 +13,7 @@ export default class App extends Component {
 
   async componentDidMount() {
     const res = await api.get('ports');
+    console.log(res);
     this.setState({
       devices: res.data
     });
@@ -22,7 +23,7 @@ export default class App extends Component {
     return (
       <div className="Devices">
           <ul className="DevicesList">
-              {this.state.devices.length === 0 ? <li></li> : this.state.devices.map(device => (
+              {this.state.devices.map(device => (
                 <li>
                   <Device id={this.state.devices.indexOf(device)} 
                           name={device.name} 
